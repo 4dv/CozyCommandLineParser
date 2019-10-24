9ce8e5f053de89f400e5f0ed861721672a83a8d2
Dmitry Chubarin <chubarin@gmail.com>
2019-10-24 18:36:11 +0300

    test ci

--- CozyCommandLineParser/Scripts/publish.sh
+++ CozyCommandLineParser/Scripts/publish.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env bash
-echo "Hi from script!"
-pwd
+
+cd ../bin/Debug
+latest=$(ls -t *.nupkg | head -n1)
+version=$(echo $latest | grep -oP '(?<=CozyCommandLineParser.).+(?=.nupkg)')
+
+echo "Latest $latest, version $version"
+
+if [[ -z "$version" ]]; then
+    echo "Can not parse version number in '$latest'"
+    exit 1
+fi
+
+tagName="release/$version"
+gitOutput=$(git tag -l "$tagName")
+echo "'$gitOutput'"
+
+if [[ -z "$gitOutput" ]]; then
+    echo "new version $version was found, set tag and publish release"
+    git tag $tagName || exit 1
+else
+    echo "tag already exist"
+fi
+
+
+#tag=$(git tag -l --contains HEAD)
+#echo "Found tag '$tag'"
+#if [[ $tag == release/* ]]; then
+#    echo "release tag exist"
+#fi
+#echo "Hi from script!"
+#pwd
 #cd ..
 #dotnet build
 #
