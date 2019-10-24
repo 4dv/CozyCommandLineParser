9b69032b5264ae0859604d13a2e80b4beddfee0f
Dmitry Chubarin <chubarin@gmail.com>
2019-10-24 17:33:46 +0300

    test ci

--- CozyCommandLineParser/Scripts/publish.sh
+++ CozyCommandLineParser/Scripts/publish.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-cd ..
-dotnet build
-
-cd bin/Debug
-latest=$(ls -t *.nupkg | head -n1)
-echo "publishing latest nupkg: $latest"
+echo "Hi from script!"
+pwd
+#cd ..
+#dotnet build
+#
+#cd bin/Debug
+#latest=$(ls -t *.nupkg | head -n1)
+#echo "publishing latest nupkg: $latest"
 #dotnet nuget push $latest -k "$GITHUB_NUGET_4DV_KEY" -s https://nuget.pkg.github.com/4dv/index.json
